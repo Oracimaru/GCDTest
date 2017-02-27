b8993a8bd57b9789d804a4a4b960a6f68d36c27e
zhb <zhanghaibin531@163.com>
2017-02-27 14:00:37 +0800

    Initial Commit

---
+++ GCDTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCDTest
+//
+//  Created by zhanghaibin on 17/2/27.
+//  Copyright © 2017年 zhanghaibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GCDTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GCDTest
+//
+//  Created by zhanghaibin on 17/2/27.
+//  Copyright © 2017年 zhanghaibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
